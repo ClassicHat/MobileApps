6aeec1abc790ab4a345e08cf1dda9fc113a1e70f
Dylan Buehler <dylanbuehler1999@gmail.com>
2021-03-14 14:38:12 -0500

    Added the web address for connecting to api

--- BookClientProject/BookClient/BookClient/BookClient/Data/BookManager.cs
+++ BookClientProject/BookClient/BookClient/BookClient/Data/BookManager.cs
@@ -1,17 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace BookClient.Data
 {
     public class BookManager
     {
-        public Task<IEnumerable<Book>> GetAll()
+        //Variables
+        static readonly string BaseAddress = "https://localhost:44318/";
+        static readonly string Url = $"{BaseAddress}/api/books/";
+        private string authorizationKey;
+
+
+        public async Task<IEnumerable<Book>> GetAll()
         {
             // TODO: use GET to retrieve books
-            throw new NotImplementedException();
-        }
+            HttpClient client = await GetClient();
+            string result = await client.GetStringAsync(Url);
+            return JsonConvert.DeserializeObject<IEnumerable<Book>>(result);
+        }//End GetAll Method
 
         public Task<Book> Add(string title, string author, string genre)
         {
@@ -30,6 +40,23 @@ namespace BookClient.Data
             // TODO: use DELETE to delete a book
             throw new NotImplementedException();
         }
-    }
-}
+
+        private async Task<HttpClient> GetClient()
+        {
+            HttpClient client = new HttpClient();
+            if (string.IsNullOrEmpty(authorizationKey))
+            {
+                authorizationKey = await client.GetStringAsync(Url + "login");
+                authorizationKey = JsonConvert.DeserializeObject<string>(authorizationKey);
+            }
+
+            client.DefaultRequestHeaders.Add("Authorization", authorizationKey);
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+            return client;
+        }//End GetClient
+
+
+
+    }//End Class
+}//End Namespace
 
